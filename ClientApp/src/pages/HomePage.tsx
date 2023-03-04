884af6e8b1a3677cf9eff621e2cefa7e1d6dff5c
Andrii Panchenko <andrevpanchenko@gmail.com>
2023-03-04 18:56:58 +0200

    fix functional components

--- ClientApp/src/pages/HomePage.tsx
+++ ClientApp/src/pages/HomePage.tsx
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import React, { type FunctionComponent, type PropsWithChildren } from 'react';
 import { Header, Main } from '../components';
 
-export class Layout extends Component {
-  static displayName = Layout.name;
-
-  render() {
-    return (
-      <div>
-        <Header  />
-          <Main />
-      </div>
-    );
-  }
+const HomePage: FunctionComponent<PropsWithChildren>  = () => {
+  return (
+    <div>
+      <Header  />
+      <Main />
+    </div>
+  )
 }
+
+export default HomePage;
