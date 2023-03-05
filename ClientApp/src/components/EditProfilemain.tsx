663876c9c9d3394a3e5ca25867301e530055d2fb
Kateryna Melnykova <kate.meln2803@gmail.com>
2023-03-05 03:34:12 +0200

    edit profile 1

--- ClientApp/src/components/EditProfilemain.tsx
+++ ClientApp/src/components/EditProfilemain.tsx
@@ -2,7 +2,10 @@ import { MDBCard, MDBCardBody, MDBContainer } from 'mdb-react-ui-kit';
 import { useState } from 'react';
 import { Badge } from 'reactstrap';
 import { UserInformation } from '../api/user.service';
-import { MDBInput } from 'mdb-react-ui-kit';
+import { MDBInput, MDBTextArea} from 'mdb-react-ui-kit';
+import { UserApi } from '../api/user.service';
+import { useEffect } from 'react';
+import { getUserId } from '../utils/storage';
 
 
 enum LanguageLevel
@@ -17,20 +20,68 @@ enum LanguageLevel
 
 export default function EditProfilemain() {
     const [userData, setUserProfile] = useState<UserInformation>();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [about, setAbout] = useState('');
+    const [cvLink, setCVLink] = useState('');
+
+    useEffect(() => {
+        Promise.all([UserApi.GetUserInfo(`${getUserId()}`)]).then(responses => {
+            const [UserInfoResponse] = responses;
+            setUserProfile({
+                id: UserInfoResponse.data.id,
+                firstName: UserInfoResponse.data.firstName,
+                lastName: UserInfoResponse.data.lastName,
+                email: UserInfoResponse.data.email,
+                skillTags: UserInfoResponse.data.skillTags,
+                about: UserInfoResponse.data.about,
+                englishLevel: UserInfoResponse.data.englishLevel,
+                cvLink: UserInfoResponse.data.cvLink
+            })
+            setFirstName(`${UserInfoResponse.data.firstName}`)
+            setLastName(`${UserInfoResponse.data.lastName}`)
+            setEmail(`${UserInfoResponse.data.email}`)
+            setAbout(`${UserInfoResponse.data.about}`)
+            setCVLink(`${UserInfoResponse.data.cvLink}`)
+        })
+    }, [])
+
+    if (userData === null) {
+        return <div>Text loading...</div>
+    }
+
+    const handleFirstName = (e: any) => {
+        setFirstName(e.target.value);
+    };
+
+    const handleLastName = (e: any) => {
+        setLastName(e.target.value);
+    };
+
+    const handleEmail = (e: any) => {
+        setEmail(e.target.value);
+    };
+
+    const handleAbout = (e: any) => {
+        setAbout(e.target.value);
+    };
+
+    const handleCV = (e: any) => {
+        setCVLink(e.target.value);
+    };
 
     return (
     <MDBContainer fluid className='align-items-center justify-content-center w-75'>
         <MDBCard className='MDBCard p-4 m-3'>
             <MDBCardBody>
-                <MDBInput wrapperClass='mb-4' label='First name' size='lg' id='form2' type='email' />
-                <h1 className='display-3 d-inline'>{`${userData?.firstName} ${userData?.lastName}`}</h1>
-                <h4 className='text-secondary lead'>{`${userData?.email}`}</h4>
+                <MDBInput wrapperClass='mb-4' onChange={handleFirstName} value={firstName} placeholder='First name' label='First name' size='lg' id='form1'  />
+                <MDBInput wrapperClass='mb-4' onChange={handleLastName} value={lastName} placeholder='Second name' label='Last name' size='lg' id='form2'  />
+                <MDBInput wrapperClass='mb-4' onChange={handleEmail} value={email} placeholder='Second name' label='Email' size='lg' id='form3'  />
                 <hr className='my-5' />
                 <h3 className='display-4 m-3'>About me:</h3>
                 <MDBCard>
-                    <MDBCardBody>
-                        {`${userData?.about}`}
-                    </MDBCardBody>
+                    <MDBTextArea onChange={handleAbout} value={about} placeholder='About me' id="form4" />
                 </MDBCard>
                 <h3 className='display-4 m-3 mt-5'>My skills: </h3>
                 <MDBCard>
@@ -43,6 +94,7 @@ export default function EditProfilemain() {
                     </MDBCardBody>
                 </MDBCard>
                 <p className='display-6 m-3 mt-5'>My CV: {`${userData?.cvLink}`}</p>
+                <MDBInput wrapperClass='mb-4' onChange={handleCV} value={cvLink} placeholder='My CV' size='lg' id='form5'  />
                 <p className='display-6 m-3'>My English level: {`${LanguageLevel[userData?.englishLevel ? userData?.englishLevel : 0]}`}</p>
             </MDBCardBody>
         </MDBCard>
