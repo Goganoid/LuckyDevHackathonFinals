a016981101dcd48476acce15c4e51dc610a5a5cd
Andrii Panchenko <andrevpanchenko@gmail.com>
2023-03-05 03:27:28 +0200

    extend popup

--- ClientApp/src/components/CreateProjectPopup.tsx
+++ ClientApp/src/components/CreateProjectPopup.tsx
@@ -1,17 +1,37 @@
 import { MDBInput } from 'mdb-react-ui-kit'
 import React, { ReactElement, useState } from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
+import Select from 'react-select';
+import { categoryOptions, convertToFilterList } from '../config/constants';
 
 export type PopupProps = {
     show: boolean,
     handleClose: () => void,
 }
 
+const tagOptions = [
+    'HTML',
+    'React',
+    '.NET'
+];
+
 export const CreateProjectPopup = ({ show, handleClose }: PopupProps) => {
+
+    const [newProject, setNewProject] = useState({
+        title: '',
+        description: '',
+        vacancyName: '',
+        vacancyDescription: '',
+        vacancyTags: [],
+        english: ''
+    });
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [vacancy, setVacancy] = useState([]);
+    const [vacancyName, setVacancyName] = useState('');
+    const [vacancyDescription, setVacancyDescription] = useState('');
+    const [vacancyTags, setVacancyTags] = useState([]);
     const [english, setEnglish] = useState<string | null>(null);
 
     const handleTitle = (e: any) => {
@@ -19,6 +39,19 @@ export const CreateProjectPopup = ({ show, handleClose }: PopupProps) => {
     };
     const handleDescription = (e: any) => {
         setDescription(e.target.value);
+    };
+    const handleVacancyName = (e: any) => {
+        setVacancyName(e.target.value);
+    };
+    const handleVacancyDescription = (e: any) => {
+        setVacancyDescription(e.target.value);
+    };
+    const handleVacancyTags = (e: any) => {
+        const value = e.value;
+        // setVacancyTags({
+        //     ...newProject,
+        //     vacancyTags: value[]
+        // });
     };
     const handleAddVacancy = () => {
         //setVacancy([]);
@@ -47,13 +80,24 @@ export const CreateProjectPopup = ({ show, handleClose }: PopupProps) => {
                 <option value="4">Upper-Intermediate</option>
                 <option value="5">Advanced</option>
             </Form.Select>
+            <br />
+            <br />
+            <MDBInput onChange={handleVacancyName} value={vacancyName} wrapperClass='mb-4' label='Your Vacancy' size='lg' id='form3' type='text' />
+            <MDBInput onChange={handleVacancyDescription} value={vacancyDescription} wrapperClass='mb-4' label='Vacancy Description' size='lg' id='form4' type='text' />
+            <Select
+                isMulti
+                options={convertToFilterList(categoryOptions)}
+                isClearable={true}
+                isSearchable={true}
+                onChange={handleVacancyTags}
+            />
         </Modal.Body>
         <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
                 Close
             </Button>
             <Button variant="primary" onClick={(e) => {
-                console.log(title, description, english);
+                console.log(title, description, english, vacancyName, vacancyDescription, vacancyTags);
             }}>
                 Save Changes
             </Button>
